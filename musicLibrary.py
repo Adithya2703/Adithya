f58078f44c26473b54f0fe8bba6aacfe918045b7
Adithya2703 <adithyakumarbh7@gmail.com>
2025-06-26 00:52:24 +0530

    Ai-Voice Assistant By python
    Name & Context: “Jarvis Virtual Assistant” — a voice‑activated desktop assistant built in Python using libraries like pyttsx3, SpeechRecognition, webbrowser, and wikipedia 
    
    
    Key Features: Understands speech, performs tasks such as:
    
    Opening websites (Google, YouTube, Instagram…)
    
    Playing music
    
    Fetching Wikipedia info
    
    Providing news/weather/time
    
    Potentially interacting with GPT APIs

---
+++ musicLibrary.py
@@ -0,0 +1,7 @@
+music = {
+    "tum": "https://www.youtube.com/watch?v=Umqb9KENgmk",             
+    "channamereya": "https://www.youtube.com/watch?v=284Ov7ysmfA",        
+    "pachtaoge": "https://www.youtube.com/watch?v=INtTooEer9c",           
+    "jhoomejopathaan": "https://www.youtube.com/watch?v=YxWlaYCA8MU",     
+    "chaleya": "https://www.youtube.com/watch?v=Muq2J3G18Ps"          
+}
